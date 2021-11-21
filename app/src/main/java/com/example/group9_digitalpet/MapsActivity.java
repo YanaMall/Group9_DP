56bc75796e7293a22f567978fee2d097576e8734
bcannoe <bcannoe@gmail.com>
2021-11-21 17:00:51 -0500

    added google maps api, MapsActivity.java, Walk.java, and activity_maps.xml

---
+++ app/src/main/java/com/example/group9_digitalpet/MapsActivity.java
@@ -0,0 +1,51 @@
+package com.example.group9_digitalpet;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.group9_digitalpet.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Just a place holder for now
+        // We need to make it so that a line is drawn on the map based on where the user walks
+        // I will do this once I figure out how and once we have more of the app complete.
+        LatLng orlando = new LatLng(-28.54878, -81.38872);
+        mMap.addMarker(new MarkerOptions().position(orlando).title("Marker in Orlando"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(orlando));
+    }
+}

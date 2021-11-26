0a5b2fcf8d69d160a3ef8cc3c5e477b572dd9c3f
bcannoe <bcannoe@gmail.com>
2021-11-26 16:14:38 -0500

    added storeActivity.java and activity_store.xml removed Store.java and Store.xml

--- app/src/main/java/com/example/group9_digitalpet/StoreActivity.java
+++ app/src/main/java/com/example/group9_digitalpet/StoreActivity.java
@@ -1,10 +1,20 @@
 package com.example.group9_digitalpet;
-// need to find a way to access currency in multiple classes.
-public class Store {
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
     private int HatPrice; // placeholder for the cost of the hat
     private int PetPrice; // placeholder for the cost of the pet
     private int Currency; // this is the currency for the entire app
-    public Store(int petPrice, int hatPrice, int currency){
+    public StoreActivity(int petPrice, int hatPrice, int currency){
         PetPrice = petPrice;
         HatPrice = hatPrice;
         Currency = currency;
@@ -35,4 +45,6 @@ public class Store {
     public void addCurrency(int amount){
         Currency += amount;
     }
+}
+
 }
